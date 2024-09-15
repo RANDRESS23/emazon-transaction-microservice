44c4aa730b07db9807007058be4c5e2388d36783
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-15 01:13:15 -0500

    feat: add driven and driving constants

---
+++ src/main/java/com/emazon/microservicio_transaccion/adapters/driving/util/DrivingConstants.java
@@ -0,0 +1,11 @@
+package com.emazon.microservicio_transaccion.adapters.driving.util;
+
+public class DrivingConstants {
+    private DrivingConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String HAS_ROLE_AUX_BODEGA = "hasRole('AUX_BODEGA')";
+    public static final String DATE_FORMAT = "dd/MM/yyyy";
+    public static final String SUPPLY_ID = "supplyId";
+}
