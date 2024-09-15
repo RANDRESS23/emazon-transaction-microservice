a1c3bfae9cc400cc736092344206efdf81951fa7
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-15 01:18:15 -0500

    feat: add necessary config for the stock microservice connection using feign client

---
+++ src/main/java/com/emazon/microservicio_transaccion/configuration/feign/IStockFeignClient.java
@@ -0,0 +1,14 @@
+package com.emazon.microservicio_transaccion.configuration.feign;
+
+import com.emazon.microservicio_transaccion.adapters.driving.dto.request.AddSupplyRequest;
+import com.emazon.microservicio_transaccion.configuration.Constants;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = Constants.STOCK_SERVICE_NAME, url = Constants.STOCK_SERVICE_URL, configuration = FeignClientInterceptor.class)
+public interface IStockFeignClient {
+    @PatchMapping("/update-quantity")
+    public ResponseEntity<Void> updateProductQuantity(@RequestBody AddSupplyRequest request);
+}
