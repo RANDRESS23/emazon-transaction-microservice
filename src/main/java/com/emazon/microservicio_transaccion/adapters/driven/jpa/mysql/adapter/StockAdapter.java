c4bf7f663de3a02c89fd1f2c14259414d52aa297
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-15 01:12:38 -0500

    feat: add stock adapter

---
+++ src/main/java/com/emazon/microservicio_transaccion/adapters/driven/jpa/mysql/adapter/StockAdapter.java
@@ -0,0 +1,18 @@
+package com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.adapter;
+
+import com.emazon.microservicio_transaccion.adapters.driving.dto.request.AddSupplyRequest;
+import com.emazon.microservicio_transaccion.configuration.feign.IStockFeignClient;
+import com.emazon.microservicio_transaccion.domain.spi.IStockPersistencePort;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class StockAdapter implements IStockPersistencePort {
+    private final IStockFeignClient stockFeignClient;
+
+    @Override
+    public void updateProductQuantity(Long productId, Long extraQuantity) {
+        AddSupplyRequest request = new AddSupplyRequest(productId, extraQuantity);
+
+        stockFeignClient.updateProductQuantity(request);
+    }
+}
