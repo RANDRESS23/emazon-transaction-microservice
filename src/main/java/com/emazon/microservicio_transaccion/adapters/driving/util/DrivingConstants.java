8fedc38fb4ee3c733530b6c3036f82fcf3e8fc91
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-20 16:34:42 -0500

    feat: add new driven and driving constants

--- src/main/java/com/emazon/microservicio_transaccion/adapters/driving/util/DrivingConstants.java
+++ src/main/java/com/emazon/microservicio_transaccion/adapters/driving/util/DrivingConstants.java
@@ -5,11 +5,12 @@ public class DrivingConstants {
         throw new IllegalStateException("Utility class");
     }
 
-    public static final String HAS_ROLE_AUX_BODEGA = "hasRole('AUX_BODEGA')";
+    public static final String HAS_ROLE_AUX_BODEGA_AND_ADMIN = "hasAnyRole('AUX_BODEGA', 'ADMIN')";
     public static final String SUPPLY_ID = "supplyId";
 
     public static final String RESPONSE_CODE_201="201";
     public static final String RESPONSE_CODE_400="400";
+    public static final String RESPONSE_CODE_404="404";
 
     public static final String TAG_SUPPLY_NAME = "Supply";
     public static final String TAG_SUPPLY_DESCRIPTION = "Supply API";
@@ -17,4 +18,10 @@ public class DrivingConstants {
     public static final String SAVE_SUPPLY_DESCRIPTION = "Creates a new supply in the database";
     public static final String SAVE_SUPPLY_RESPONSE_201_DESCRIPTION = "Supply created successfully";
     public static final String SAVE_SUPPLY_RESPONSE_400_DESCRIPTION = "Invalid input";
+
+    public static final String GET_SUPPLY_SUMMARY = "Get supply";
+    public static final String GET_SUPPLY_DESCRIPTION = "Recover a supply";
+    public static final String GET_SUPPLY_RESPONSE_201_DESCRIPTION = "Supply retrieved successfully";
+    public static final String GET_SUPPLY_RESPONSE_400_DESCRIPTION = "Invalid param";
+    public static final String GET_SUPPLY_RESPONSE_404_DESCRIPTION = "Supply not found";
 }
