a0c2dc8177cf11595681c5ff0285960340280a61
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-27 16:46:38 -0500

    feat: add sale use case

---
+++ src/main/java/com/emazon/microservicio_transaccion/domain/api/usecase/SaleUseCase.java
@@ -0,0 +1,30 @@
+package com.emazon.microservicio_transaccion.domain.api.usecase;
+
+import com.emazon.microservicio_transaccion.domain.api.ISaleServicePort;
+import com.emazon.microservicio_transaccion.domain.model.ProductSold;
+import com.emazon.microservicio_transaccion.domain.model.Sale;
+import com.emazon.microservicio_transaccion.domain.spi.IProductSoldPersistencePort;
+import com.emazon.microservicio_transaccion.domain.spi.ISalePersistencePort;
+
+import java.util.List;
+
+public class SaleUseCase implements ISaleServicePort {
+    private final ISalePersistencePort salePersistencePort;
+    private final IProductSoldPersistencePort productSoldPersistencePort;
+
+    public SaleUseCase(ISalePersistencePort salePersistencePort, IProductSoldPersistencePort productSoldPersistencePort) {
+        this.salePersistencePort = salePersistencePort;
+        this.productSoldPersistencePort = productSoldPersistencePort;
+    }
+
+    @Override
+    public void saveSale(Sale sale) {
+        Sale saleSaved = salePersistencePort.saveSale(sale);
+        List<ProductSold> productsSold = sale.getProducts();
+
+        productsSold.forEach(productSold -> {
+            productSold.setSaleId(saleSaved.getSaleId());
+            productSoldPersistencePort.saveProductSold(productSold);
+        });
+    }
+}
