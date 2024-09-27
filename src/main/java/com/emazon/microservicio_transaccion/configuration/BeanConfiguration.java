7456d1ff160baaf4e306c5efc681230b86f4d9da
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-27 16:48:14 -0500

    feat: add new beans configuration

--- src/main/java/com/emazon/microservicio_transaccion/configuration/BeanConfiguration.java
+++ src/main/java/com/emazon/microservicio_transaccion/configuration/BeanConfiguration.java
@@ -1,13 +1,17 @@
 package com.emazon.microservicio_transaccion.configuration;
 
-import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.adapter.AuthAdapter;
-import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.adapter.StateAdapter;
-import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.adapter.StockAdapter;
-import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.adapter.SupplyAdapter;
+import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.adapter.*;
+import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.mapper.IProductSoldEntityMapper;
+import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.mapper.ISaleEntityMapper;
 import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.mapper.IStateEntityMapper;
 import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.mapper.ISupplyEntityMapper;
+import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.repository.IProductSoldRepository;
+import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.repository.ISaleRepository;
 import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.repository.IStateRepository;
 import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.repository.ISupplyRepository;
+import com.emazon.microservicio_transaccion.domain.api.ISaleServicePort;
+import com.emazon.microservicio_transaccion.domain.api.usecase.SaleUseCase;
+import com.emazon.microservicio_transaccion.domain.spi.*;
 import com.emazon.microservicio_transaccion.domain.validation.ValidationFailureMessage;
 import com.emazon.microservicio_transaccion.configuration.feign.IStockFeignClient;
 import com.emazon.microservicio_transaccion.configuration.securityconfig.jwtconfiguration.JwtService;
@@ -15,10 +19,6 @@ import com.emazon.microservicio_transaccion.domain.api.IStateServicePort;
 import com.emazon.microservicio_transaccion.domain.api.ISupplyServicePort;
 import com.emazon.microservicio_transaccion.domain.api.usecase.StateUseCase;
 import com.emazon.microservicio_transaccion.domain.api.usecase.SupplyUseCase;
-import com.emazon.microservicio_transaccion.domain.spi.IAuthPersistencePort;
-import com.emazon.microservicio_transaccion.domain.spi.IStatePersistencePort;
-import com.emazon.microservicio_transaccion.domain.spi.IStockPersistencePort;
-import com.emazon.microservicio_transaccion.domain.spi.ISupplyPersistencePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,7 +29,11 @@ public class BeanConfiguration {
     private final ISupplyRepository supplyRepository;
     private final ISupplyEntityMapper supplyEntityMapper;
     private final IStateRepository stateRepository;
+    private final ISaleRepository saleRepository;
+    private final IProductSoldRepository productSoldRepository;
     private final IStateEntityMapper stateEntityMapper;
+    private final ISaleEntityMapper saleEntityMapper;
+    private final IProductSoldEntityMapper productSoldEntityMapper;
     private final JwtService jwtService;
     private final IStockFeignClient stockFeignClient;
 
@@ -66,5 +70,20 @@ public class BeanConfiguration {
     @Bean
     public IStateServicePort stateServicePort() {
         return new StateUseCase(statePersistencePort());
+    }
+
+    @Bean
+    public ISalePersistencePort salePersistencePort() {
+        return new SaleAdapter(saleRepository, saleEntityMapper);
+    }
+
+    @Bean
+    public IProductSoldPersistencePort productSoldPersistencePort() {
+        return new ProductSoldAdapter(productSoldRepository, productSoldEntityMapper);
+    }
+
+    @Bean
+    public ISaleServicePort saleServicePort() {
+        return new SaleUseCase(salePersistencePort(), productSoldPersistencePort());
     }
 }
