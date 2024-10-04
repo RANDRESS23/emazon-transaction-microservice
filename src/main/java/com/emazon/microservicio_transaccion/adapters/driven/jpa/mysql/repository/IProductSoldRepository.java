194d1228c487e137813710ec29589c212f9fa09f
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-04 10:33:08 -0500

    feat: add new custom queries in the sale and product sold repositories

--- src/main/java/com/emazon/microservicio_transaccion/adapters/driven/jpa/mysql/repository/IProductSoldRepository.java
+++ src/main/java/com/emazon/microservicio_transaccion/adapters/driven/jpa/mysql/repository/IProductSoldRepository.java
@@ -2,6 +2,12 @@ package com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.repositor
 
 import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.entity.ProductSoldEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IProductSoldRepository extends JpaRepository<ProductSoldEntity, Long> {
+    @Modifying
+    @Query(value = "DELETE FROM products_sold WHERE sale_id = :saleId", nativeQuery = true)
+    void removeAllCartProductsSold(@Param("saleId") Long saleId);
 }
--- src/main/java/com/emazon/microservicio_transaccion/adapters/driven/jpa/mysql/repository/ISaleRepository.java
+++ src/main/java/com/emazon/microservicio_transaccion/adapters/driven/jpa/mysql/repository/ISaleRepository.java
@@ -2,6 +2,12 @@ package com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.repositor
 
 import com.emazon.microservicio_transaccion.adapters.driven.jpa.mysql.entity.SaleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ISaleRepository extends JpaRepository<SaleEntity, Long> {
+    @Modifying
+    @Query(value = "DELETE FROM sale WHERE sale_id = :saleId", nativeQuery = true)
+    void removeSale(@Param("saleId") Long saleId);
 }
